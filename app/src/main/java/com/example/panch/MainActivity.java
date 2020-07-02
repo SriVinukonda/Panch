8256e030e85c2190d201a70faa2d4a02217b1e59
SirHappi <sri13.vinukonda@gmail.com>
2020-07-02 13:27:26 -0400

    Added cool background to Main activity

--- app/src/main/java/com/example/panch/MainActivity.java
+++ app/src/main/java/com/example/panch/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.panch;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,14 +10,19 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
         Button populate  = findViewById(R.id.button);
 
+        ConstraintLayout constraintLayout = findViewById(R.id.main_act);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(0);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+
+
         final Intent intent = new Intent(this, MapsActivity.class);
         populate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 startActivity(intent);
             }
         });
 
 
     }
-
-
 
 
 }
